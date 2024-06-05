d5636a1fc1b1e859bc49829cab76c3a7461cb75f
adrianEffe <adrian.iosdev@gmail.com>
2024-06-05 17:51:29 +0200

    add healthcheck

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/api/healthcheck", get(healthcheck));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn healthcheck() -> String {
+    "Hello World".to_string()
 }
