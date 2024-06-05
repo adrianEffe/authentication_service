c7d61f95520ffc974ffd86e76ddd6d51a00b1dec
adrianEffe <adrian.iosdev@gmail.com>
2024-06-05 21:51:50 +0200

    add api module with respective utils

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
-mod utils;
-use axum::{response::IntoResponse, routing::get, Json, Router};
-use utils::status::{response_message, Status};
+mod api;
+use api::healthcheck::healthcheck;
+use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/api/healthcheck", get(healthcheck));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
-}
-
-async fn healthcheck() -> impl IntoResponse {
-    Json(response_message(&Status::Success, "Hello world!!"))
 }
