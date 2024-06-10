1e27806d253696afd8f5f1357fd6e72ef3a2e268
adrianEffe <adrian.iosdev@gmail.com>
2024-06-10 20:23:01 +0200

    move app logic to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-mod api;
-use api::healthcheck::healthcheck;
+use authentication_service::api::utils::status::Status;
+use authentication_service::app;
 use axum::{routing::get, Router};
 use serde::Deserialize;
 use tokio::net::TcpListener;
@@ -8,10 +8,6 @@ use tokio::net::TcpListener;
 async fn main() {
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app()).await.unwrap();
-}
-
-fn app() -> Router {
-    Router::new().route("/api/healthcheck", get(healthcheck))
 }
 
 #[tokio::test]
@@ -28,11 +24,11 @@ async fn test_healthcheck() {
     let body = reqwest::get(url).await.unwrap().text().await.unwrap();
     let deserialised: StatusResponse = serde_json::from_str(&body).unwrap();
 
-    assert_eq!(deserialised.status, api::utils::status::Status::Success);
+    assert_eq!(deserialised.status, Status::Success);
 }
 
 #[cfg(test)]
 #[derive(Deserialize)]
 struct StatusResponse {
-    status: api::utils::status::Status,
+    status: Status,
 }
