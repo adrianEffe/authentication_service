e78a01dca1ff913d6dc5e83a7cac2657216a777c
adrianEffe <adrian.iosdev@gmail.com>
2024-06-12 18:07:29 +0200

    fix typo in up migration

--- migrations/20240612153749_init.up.sql
+++ migrations/20240612153749_init.up.sql
@@ -5,9 +5,9 @@ CREATE TABLE
 	"users" (
 	id UUID NOT NULL PRIMARY KEY DEFAULT (uuid_generate_v4()),
 	email VARCHAR(255) NOT NULL UNIQUE,
-	password: VARCHAR(100) NOT NULL,
+	password VARCHAR(100) NOT NULL,
 	created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-	updated_at TIMESTMP WITH TIME ZONE DEFAULT NOW()
+	updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 	);
 
 CREATE INDEX users_email_idx ON users (email);
