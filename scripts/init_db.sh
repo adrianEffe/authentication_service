9cea803c878c318b0c5160f9e3ca092c283447ab
adrianEffe <adrian.iosdev@gmail.com>
2024-06-13 16:28:43 +0200

    add init_db.sh script

---
+++ scripts/init_db.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+set -x
+set -eo pipefail
+# check if postgres cli is installed
+if ! [ -x "$(command -v psql)" ]; then
+  echo >&2 "Error: psql is not installed."
+  exit 1
+fi
+
+# check if sqlx cli is installed
+if ! [ -x "$(command -v sqlx)" ]; then
+  echo >&2 "Error: sqlx is not installed."
+  echo >&2 "Use:"
+  echo >&2 "    cargo install --version='~0.7.4' sqlx-cli --no-default-features --features rustls,postgres"
+  echo >&2 "to install it."
+  exit 1
+fi
+
+# Allow to skip Docker if a dockerized Postgres database is already running
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+  # if a postgres container is running, print instructions to kill it and exit
+  RUNNING_POSTGRES_CONTAINER=$(docker ps --filter 'name=postgres' --format '{{.ID}}')
+  if [[ -n $RUNNING_POSTGRES_CONTAINER ]]; then
+    echo >&2 "there is a postgres container already running, kill it with"
+    echo >&2 "    docker kill ${RUNNING_POSTGRES_CONTAINER}"
+    exit 1
+  fi
+
+  docker-compose up -d postgres
+fi
+
+# Load environment variables from .env file
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+fi
+
+# Keep pinging Postgres until it's ready to accept connections
+until pg_isready -h "${POSTGRES_HOST}" -p "${POSTGRES_PORT}"; do
+  >&2 echo "Postgres is still unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up and running on port ${POSTGRES_PORT} - running migrations now!"
+
+# SQLX
+sqlx database create
+sqlx migrate run
+
+cargo sqlx prepare -- --tests
+
+>&2 echo "Postgres has been migrated, ready to go!"
