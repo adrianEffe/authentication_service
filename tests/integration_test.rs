35ebfe51d0d24ffd0450420a99475ed572507e23
adrianEffe <adrian.iosdev@gmail.com>
2024-06-17 15:29:06 +0200

    move assert after clean up

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -15,21 +15,32 @@ async fn test_register() {
     let address = spawn_server().await;
 
     let url = format!("http://{}/api/register", address);
+    let client = reqwest::Client::new();
 
-    let response: GenericResponse<UserData> =
-        reqwest::get(url).await.unwrap().json().await.unwrap();
+    let email = "email@test.com";
+    let body = serde_json::json!({
+        "email": email,
+        "password": "12345678"
+    });
 
-    assert_eq!(response.data.unwrap().user.email, "adrian@email.com");
-
-    clean_up_db(|db| async move {
-        db.execute(sqlx::query!(
-            "DELETE FROM users WHERE email = $1",
-            "adrian@email.com".to_string()
-        ))
+    let response: GenericResponse<UserData> = client
+        .post(&url)
+        .json(&body)
+        .send()
+        .await
+        .unwrap()
+        .json()
         .await
         .unwrap();
+
+    clean_up_db(|db| async move {
+        db.execute(sqlx::query!("DELETE FROM users WHERE email = $1", email))
+            .await
+            .unwrap();
     })
     .await;
+
+    assert_eq!(response.data.unwrap().user.email, email);
 }
 
 #[tokio::test]
