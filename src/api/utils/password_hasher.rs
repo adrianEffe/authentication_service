86ac9606359d5b64a083e86dbf912b4066318842
adrianEffe <adrian.iosdev@gmail.com>
2024-06-18 17:25:46 +0200

    add password hasher module

---
+++ src/api/utils/password_hasher.rs
@@ -0,0 +1,42 @@
+use argon2::{
+    password_hash::Error, password_hash::SaltString, Argon2, PasswordHash, PasswordHasher,
+    PasswordVerifier,
+};
+use rand_core::OsRng;
+
+pub fn hash_password(password: &str) -> Result<String, Error> {
+    let salt = SaltString::generate(&mut OsRng);
+    let hashed_password = Argon2::default()
+        .hash_password(password.as_bytes(), &salt)
+        .map(|hash| hash.to_string());
+    hashed_password
+}
+
+pub fn is_valid(password: &str, hashed_password: &str) -> bool {
+    match PasswordHash::new(hashed_password) {
+        Ok(parsed_hash) => Argon2::default()
+            .verify_password(password.as_bytes(), &parsed_hash)
+            .map_or(false, |_| true),
+        Err(_) => false,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_valid_password_hashing() {
+        let password = "password".to_string();
+        let hashed_password = hash_password(&password).unwrap();
+
+        assert!(is_valid(&password, &hashed_password));
+    }
+
+    #[test]
+    fn test_invalid_password_hashing() {
+        let hashed_password = hash_password("password").unwrap();
+
+        assert!(!is_valid("1234", &hashed_password));
+    }
+}
