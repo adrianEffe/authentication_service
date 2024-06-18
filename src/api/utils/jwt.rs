b610dab03524cd6241b52d6feba060f4d288e2fd
adrianEffe <adrian.iosdev@gmail.com>
2024-06-18 14:29:29 +0200

    add validation method

--- src/api/utils/jwt.rs
+++ src/api/utils/jwt.rs
@@ -2,6 +2,29 @@ use crate::model::token::{TokenClaims, TokenDetails};
 use anyhow::Result;
 use base64::{engine::general_purpose, Engine};
 
+pub fn verify_jwt(public_key: String, token: &str) -> Result<TokenDetails> {
+    let bytes_public_key = general_purpose::STANDARD.decode(public_key)?;
+    let decoded_public_key = String::from_utf8(bytes_public_key)?;
+
+    let validation = jsonwebtoken::Validation::new(jsonwebtoken::Algorithm::RS256);
+
+    let decoded = jsonwebtoken::decode::<TokenClaims>(
+        token,
+        &jsonwebtoken::DecodingKey::from_rsa_pem(decoded_public_key.as_bytes())?,
+        &validation,
+    )?;
+
+    let user_id = uuid::Uuid::parse_str(decoded.claims.sub.as_str())?;
+    let token_uuid = uuid::Uuid::parse_str(decoded.claims.token_uuid.as_str())?;
+
+    Ok(TokenDetails {
+        token: None,
+        token_uuid,
+        user_id,
+        expires_in: None,
+    })
+}
+
 pub fn generate_jwt(user_id: uuid::Uuid, ttl: i64, private_key: String) -> Result<TokenDetails> {
     let bytes_private_key = general_purpose::STANDARD.decode(private_key)?;
     let decoded_private_key = String::from_utf8(bytes_private_key)?;
