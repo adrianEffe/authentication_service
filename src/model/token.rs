f171797b1b0a5f345ab6b30aec4ce934d5b7e12f
adrianEffe <adrian.iosdev@gmail.com>
2024-06-18 11:35:55 +0200

    add token claim struct

---
+++ src/model/token.rs
@@ -0,0 +1,10 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TokenClaims {
+    pub sub: String,
+    pub token_uuid: String,
+    pub exp: i64,
+    pub iat: i64,
+    pub nbf: i64,
+}
