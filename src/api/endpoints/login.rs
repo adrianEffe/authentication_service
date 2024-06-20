aaa8a16bccd8fa7c3fce64470ab79f4ce90b70af
adrianEffe <adrian.iosdev@gmail.com>
2024-06-20 11:51:46 +0200

    remove unwraps and refactor

--- src/api/endpoints/login.rs
+++ src/api/endpoints/login.rs
@@ -25,63 +25,78 @@ pub async fn login_handler(
     Json(body): Json<LoginUserSchema>,
 ) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
     let user = fetch_user(&data.db, &body.email).await?;
-    // TODO: extract validate password
+
     let is_valid = is_valid(&body.password, &user.password);
     if !is_valid {
         let error_response = response_message(&Status::Failure, "Invalid email or password");
         return Err((StatusCode::BAD_REQUEST, Json(error_response)));
     }
 
-    let access_token_details = generate_jwt(
+    let access_token = generate_access_token(
         user.id,
         data.env.access_token_max_age,
         &data.env.access_token_private_key,
-    )
-    .map_err(|_| {
-        let error_message = response_message(&Status::Failure, "Failed to generate token");
-        (StatusCode::INTERNAL_SERVER_ERROR, Json(error_message))
-    })?;
+    )?;
 
-    //TODO: extract cookie builder function
+    let mut response =
+        Response::new(response_data(&Status::Success, "access_token", &access_token).to_string());
 
-    // Remove unwraps
-    let access_cookie = Cookie::build((
-        "access_token",
-        access_token_details.token.clone().unwrap_or_default(),
-    ))
-    .path("/")
-    .max_age(time::Duration::minutes(data.env.access_token_max_age))
-    .same_site(SameSite::Lax)
-    .http_only(true);
-
-    let logged_in_cookie = Cookie::build(("logged_in", "true"))
-        .path("/")
-        .max_age(time::Duration::minutes(data.env.access_token_max_age))
-        .same_site(SameSite::Lax)
-        .http_only(false);
-
-    // remove unwraps
-    let mut response = Response::new(
-        response_data(
-            &Status::Success,
-            "access_token",
-            access_token_details.token.unwrap(),
-        )
-        .to_string(),
-    );
-
-    let mut headers = HeaderMap::new();
-    headers.append(
-        header::SET_COOKIE,
-        access_cookie.to_string().parse().unwrap(),
-    );
-    headers.append(
-        header::SET_COOKIE,
-        logged_in_cookie.to_string().parse().unwrap(),
-    );
+    let headers = set_cookies_in_header(&access_token, data.env.access_token_max_age)?;
 
     response.headers_mut().extend(headers);
     Ok(response)
+}
+
+fn generate_access_token(
+    user_id: uuid::Uuid,
+    max_age: i64,
+    private_key: &str,
+) -> Result<String, (StatusCode, Json<serde_json::Value>)> {
+    generate_jwt(user_id, max_age, private_key)
+        .map_err(|_| {
+            let error_message = response_message(&Status::Failure, "Failed to generate jwt detail");
+            (StatusCode::INTERNAL_SERVER_ERROR, Json(error_message))
+        })?
+        .token
+        .ok_or_else(|| {
+            let error_message = response_message(&Status::Failure, "Failed to generate token");
+            (StatusCode::INTERNAL_SERVER_ERROR, Json(error_message))
+        })
+}
+
+fn set_cookies_in_header(
+    access_token: &str,
+    max_age: i64,
+) -> Result<HeaderMap, (StatusCode, Json<serde_json::Value>)> {
+    let access_cookie = Cookie::build(("access_token", access_token))
+        .path("/")
+        .max_age(time::Duration::minutes(max_age))
+        .same_site(SameSite::Lax)
+        .http_only(true)
+        .to_string()
+        .parse()
+        .map_err(|_| {
+            let error_message = response_message(&Status::Failure, "Failed to parse access cookie");
+            (StatusCode::INTERNAL_SERVER_ERROR, Json(error_message))
+        })?;
+
+    let logged_in_cookie = Cookie::build(("logged_in", "true"))
+        .path("/")
+        .max_age(time::Duration::minutes(max_age))
+        .same_site(SameSite::Lax)
+        .http_only(false)
+        .to_string()
+        .parse()
+        .map_err(|_| {
+            let error_message =
+                response_message(&Status::Failure, "Failed to parse logged in cookie");
+            (StatusCode::INTERNAL_SERVER_ERROR, Json(error_message))
+        })?;
+
+    let mut headers = HeaderMap::new();
+    headers.append(header::SET_COOKIE, access_cookie);
+    headers.append(header::SET_COOKIE, logged_in_cookie);
+    Ok(headers)
 }
 
 async fn fetch_user(
