80ed3097cd821ea3b619fadb65ff400671d7a59d
adrianEffe <adrian.iosdev@gmail.com>
2024-06-21 11:41:41 +0200

    verify redis active session with token in middleware

--- src/api/middlewares/authentication.rs
+++ src/api/middlewares/authentication.rs
@@ -4,7 +4,7 @@ use crate::{
         status::{response_message, Status},
     },
     application::AppState,
-    model::{auth_middleware::AuthMiddleware, user::User},
+    model::{auth_middleware::AuthMiddleware, token::TokenDetails, user::User},
 };
 use axum::{
     body::Body,
@@ -15,6 +15,7 @@ use axum::{
     Json,
 };
 use axum_extra::extract::CookieJar;
+use redis::{AsyncCommands, Client};
 use sqlx::{Pool, Postgres};
 use std::sync::Arc;
 
@@ -34,7 +35,7 @@ pub async fn auth(
         }
     };
 
-    // TODO: Could have a redis instance to check for revoked tokens
+    verify_active_session(&data.redis, &access_token_details).await?;
 
     let user = fetch_user_from_db(&data.db, access_token_details.user_id).await?;
 
@@ -43,6 +44,36 @@ pub async fn auth(
         access_token_uuid: access_token_details.token_uuid,
     });
     Ok(next.run(req).await)
+}
+
+async fn verify_active_session(
+    redis: &Client,
+    access_token_details: &TokenDetails,
+) -> Result<(), (StatusCode, Json<serde_json::Value>)> {
+    let access_token_uuid = uuid::Uuid::parse_str(&access_token_details.token_uuid.to_string())
+        .map_err(|_| {
+            let error_response = response_message(&Status::Failure, "Invalid token");
+            (StatusCode::UNAUTHORIZED, Json(error_response))
+        })?;
+
+    let mut redis_client = redis
+        .get_multiplexed_async_connection()
+        .await
+        .map_err(|e| {
+            let message = format!("Redis error: {}", e);
+            let error_response = response_message(&Status::Failure, &message);
+            (StatusCode::UNAUTHORIZED, Json(error_response))
+        })?;
+
+    redis_client
+        .get::<_, String>(access_token_uuid.to_string())
+        .await
+        .map_err(|_| {
+            let error_response =
+                response_message(&Status::Failure, "Token is invalid or session has expired");
+            (StatusCode::UNAUTHORIZED, Json(error_response))
+        })?;
+    Ok(())
 }
 
 async fn fetch_user_from_db(
