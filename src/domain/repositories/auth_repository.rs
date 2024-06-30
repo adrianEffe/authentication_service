11b17c64d4820fa50b1040eaa2f28f36d0e5f4ff
adrianEffe <adrian.iosdev@gmail.com>
2024-06-30 16:48:46 +0200

    add domain definition for auth repository

---
+++ src/domain/repositories/auth_repository.rs
@@ -0,0 +1,10 @@
+use crate::model::register_user::{RegisterUserError, RegisterUserRequest};
+use crate::model::user::FilteredUser;
+use std::future::Future;
+
+pub trait AuthRepository: Send + Sync + 'static {
+    fn register(
+        &self,
+        request: &RegisterUserRequest,
+    ) -> impl Future<Output = Result<FilteredUser, RegisterUserError>> + Send;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod api;
 pub mod application;
+pub mod domain;
 pub mod helper;
 pub mod model;
-
+pub mod repositories;
