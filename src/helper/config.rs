b03b669234afb05d83aa27ca93d2a852f2e416e1
adrianEffe <adrian.iosdev@gmail.com>
2024-07-04 15:13:05 +0200

    add refresh token variables to config

--- src/helper/config.rs
+++ src/helper/config.rs
@@ -5,6 +5,10 @@ pub struct Config {
     pub access_token_public_key: String,
     pub access_token_expires_in: String,
     pub access_token_max_age: i64,
+    pub refresh_token_private_key: String,
+    pub refresh_token_public_key: String,
+    pub refresh_token_expires_in: String,
+    pub refresh_token_max_age: i64,
     pub redis_url: String,
 }
 
@@ -19,6 +23,10 @@ impl Config {
         let access_token_public_key = get_env("ACCESS_TOKEN_PUBLIC_KEY");
         let access_token_expires_in = get_env("ACCESS_TOKEN_EXPIRES_IN");
         let access_token_max_age = get_env("ACCESS_TOKEN_MAXAGE");
+        let refresh_token_private_key = get_env("REFRESH_TOKEN_PRIVATE_KEY");
+        let refresh_token_public_key = get_env("REFRESH_TOKEN_PUBLIC_KEY");
+        let refresh_token_expires_in = get_env("REFRESH_TOKEN_EXPIRES_IN");
+        let refresh_token_max_age = get_env("REFRESH_TOKEN_MAXAGE");
         let redis_url = format!(
             "redis://{}:{}",
             get_env("REDIS_HOST"),
@@ -30,7 +38,15 @@ impl Config {
             access_token_private_key,
             access_token_public_key,
             access_token_expires_in,
-            access_token_max_age: access_token_max_age.parse::<i64>().unwrap(),
+            access_token_max_age: access_token_max_age
+                .parse::<i64>()
+                .expect("Access token max age failed to parse from .env"),
+            refresh_token_private_key,
+            refresh_token_public_key,
+            refresh_token_expires_in,
+            refresh_token_max_age: refresh_token_max_age
+                .parse::<i64>()
+                .expect("Refresh token max age failed to parse from .env"),
             redis_url,
         }
     }
