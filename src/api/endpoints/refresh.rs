2a1cefddf4592a245885988a13fbbb6a60b9be5c
adrianEffe <adrian.iosdev@gmail.com>
2024-07-05 17:16:44 +0200

    wrap up refresh handle

--- src/api/endpoints/refresh.rs
+++ src/api/endpoints/refresh.rs
@@ -3,28 +3,44 @@ use std::sync::Arc;
 use anyhow::{anyhow, Result};
 use axum::{
     extract::State,
-    response::{IntoResponse, Response},
+    http::{header, HeaderMap, Response},
+    response::IntoResponse,
 };
-use axum_extra::extract::CookieJar;
+use axum_extra::extract::{
+    cookie::{Cookie, SameSite},
+    CookieJar,
+};
 
 use crate::{
-    api::model::api_error::ApiError,
+    api::model::{api_error::ApiError, api_response::ApiResponse},
     application::AppState,
-    domain::{auth_service::AuthService, model::refresh_token::RefreshRequest},
+    domain::{
+        auth_service::AuthService,
+        model::refresh_token::{RefreshRequest, RefreshResponse, RefreshTokenError},
+    },
 };
 
 pub async fn refresh_access_token_handler<AS: AuthService>(
     cookie_jar: CookieJar,
     State(state): State<Arc<AppState<AS>>>,
 ) -> Result<impl IntoResponse, ApiError> {
-    let refresh_token = extract_refresh_token(cookie_jar)
-        // .map_err(RefreshTokenError::MissingCredentials)
-        .unwrap(); //TODO: //handle error
+    let refresh_token =
+        extract_refresh_token(cookie_jar).map_err(|_| RefreshTokenError::MissingCredentials)?;
+
     let domain_request = RefreshRequest::new(refresh_token);
 
-    let access_token = state.auth_service.refresh(&domain_request).await;
+    let refresh_response = state.auth_service.refresh(&domain_request).await?;
 
-    Ok(Response::new("todo".to_string()))
+    let headers = set_cookies_in_header(&refresh_response).map_err(|e| {
+        ApiError::from(RefreshTokenError::Unknown(
+            anyhow!(e).context("Failed to set cookies in header"),
+        ))
+    })?;
+
+    let mut response = Response::new(ApiResponse::success(refresh_response).to_json().to_string());
+    response.headers_mut().extend(headers);
+
+    Ok(response)
 }
 
 fn extract_refresh_token(cookie_jar: CookieJar) -> Result<String> {
@@ -32,4 +48,27 @@ fn extract_refresh_token(cookie_jar: CookieJar) -> Result<String> {
         .get("refresh_token")
         .map(|cookie| cookie.value().to_string())
         .ok_or(anyhow!("Failed to extract refresh token from cookie"))
+}
+
+fn set_cookies_in_header(details: &RefreshResponse) -> anyhow::Result<HeaderMap> {
+    let access_cookie = Cookie::build(("access_token", details.access_token.to_string()))
+        .path("/")
+        .max_age(time::Duration::minutes(details.access_token_max_age))
+        .same_site(SameSite::Lax)
+        .http_only(true)
+        .to_string()
+        .parse()?;
+
+    let logged_in_cookie = Cookie::build(("logged_in", "true"))
+        .path("/")
+        .max_age(time::Duration::minutes(details.access_token_max_age))
+        .same_site(SameSite::Lax)
+        .http_only(false)
+        .to_string()
+        .parse()?;
+
+    let mut headers = HeaderMap::new();
+    headers.append(header::SET_COOKIE, access_cookie);
+    headers.append(header::SET_COOKIE, logged_in_cookie);
+    Ok(headers)
 }
