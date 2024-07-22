6a56d02f6bb5d0e55df2d5a01062d26845a1a5a2
adrianEffe <adrian.iosdev@gmail.com>
2024-07-22 14:46:56 +0200

    document auth repository trait

--- src/domain/repositories/auth_repository.rs
+++ src/domain/repositories/auth_repository.rs
@@ -7,6 +7,22 @@ use crate::domain::model::{
 };
 use std::future::Future;
 
+/// Trait defining the contract for authentication-related database repository operations.
+///
+/// The `AuthRepository` trait specifies the necessary methods for user registration,
+/// login, and fetching user details by ID. Implementing this trait allows for
+/// interaction with various data storage backends.
+///
+/// # Requirements
+///
+/// Any struct that implements the `AuthRepository` trait must be `Send`, `Sync`,
+/// and have a `'static` lifetime. This ensures that instances of the implementing
+/// struct can be safely shared across threads and have a static lifetime.
+///
+/// # Errors
+///
+/// The methods in this trait return a `Result` with the associated data type on success
+/// or an `AuthRepositoryError` on failure.
 pub trait AuthRepository: Send + Sync + 'static {
     fn register(
         &self,
